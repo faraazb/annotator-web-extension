420fac26cee3029c9eca8cb1fc8a30e71110a7e3
Faraaz Biyabani <faraaz.biyabani@contentstack.com>
2023-04-03 00:42:35 +0530

    Add toast notifications displaying annotations submit status

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
 import { h, render } from "preact";
+import { Toaster } from "react-hot-toast";
 import Tools from "./components/tools";
 import styles from "bundle-text:./app.css";
 
@@ -10,13 +11,19 @@ const initApp = () => {
     const toolsContainer = document.createElement("div");
     toolsContainer.id = "annotator-tools-container";
     document.body.appendChild(toolsContainer);
-    
+
     toolsContainer.attachShadow({ mode: "open" });
     const style = document.createElement("style");
     style.innerHTML = styles;
     toolsContainer.shadowRoot.appendChild(style);
 
     render(<Tools />, toolsContainer.shadowRoot);
+    render(
+        <Toaster
+            containerStyle={{ position: "fixed" }}
+        />,
+        appContainer
+    );
 };
 
 export { initApp };
